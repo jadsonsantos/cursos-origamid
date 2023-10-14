110407e1cdd76fcafc8601e3b414d07ef9cd9e7c
Jadson Santos <jadsondossantos5@gmail.com>
2023-10-14 11:43:06 -0300

    exercise

--- typescript/exercicios/app.ts
+++ typescript/exercicios/app.ts
@@ -2,3 +2,10 @@ let idade: number = 15;
 let nome: string = 'Jadson';
 
 console.log(`nome: ${nome}, idade: ${idade}`);
+
+function normalizarTexto(texto: string) {
+  return texto.trim().toLowerCase();
+}
+
+console.log(normalizarTexto(" JADSON "));
+
